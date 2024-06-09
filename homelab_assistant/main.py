8a29c193b150b0096c50339cb0124989ad1e2f83
Twalaght <52785900+Twalaght@users.noreply.github.com>
2024-06-09 19:32:14 +0930

    Improved sync functionality
    - Now operates only on file compose files
    - Git syncing now handled by program, not Portainer
    - Implement config object dataclass
    - Improve main runner interface

--- homelab_assistant/main.py
+++ homelab_assistant/main.py
@@ -3,45 +3,66 @@
 import argparse
 from pathlib import Path
 
+import cattrs
 import yaml
 from rich.console import Console
 
+from homelab_assistant.config import Config
 from homelab_assistant.portainer import PortainerHelper
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--export", metavar="EXPORT_FILE",
-                    help="Export current Portainer environment to a file.")
-parser.add_argument("-s", "--sync", default=None,
-                    choices=["file", "git", "all"],
-                    help="Type of Portainer stack to synchronise with config defined environment.")
-parser.add_argument("config", metavar="CONFIG_FILE",
-                    help="Config file defining common and stack specific environment variables")
+
+def parent_parser() -> argparse.ArgumentParser:
+    """ Template for bottom level "leaf" parsers. """
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-c", "--config", required=True,
+                        help="YAML file to source configuration data from.")
+    return parser
+
+
+def export(args: argparse.Namespace, config: Config, portainer_connector: PortainerHelper) -> None:
+    """ `export` entrypoint runner. """
+    del config  # Specific config is not used in export
+
+    stack_data = portainer_connector.export_config_from_stacks()
+    with Path(args.export_file).open("w") as f:
+        yaml.dump(stack_data, f, indent=4)
+
+
+def sync(args: argparse.Namespace, config: Config, portainer_connector: PortainerHelper) -> None:
+    """ `sync` entrypoint runner. """
+    del args  # Specific args are not used for sync
+
+    # TODO - Options to dry run?
+    portainer_connector.sync_stacks(config)
+
+
+__ROOT_PARSER = argparse.ArgumentParser(add_help=True)
+__ROOT_SUBPARSERS = __ROOT_PARSER.add_subparsers(required=True)
+
+EXPORT_PARSER = __ROOT_SUBPARSERS.add_parser("export", parents=[parent_parser()])
+EXPORT_PARSER.set_defaults(entry_func=export)
+EXPORT_PARSER.add_argument("export_file")
+
+SYNC_PARSER = __ROOT_SUBPARSERS.add_parser("sync", parents=[parent_parser()])
+SYNC_PARSER.set_defaults(entry_func=sync)
 
 console = Console()
 
 
 def main() -> None:
     """ Entrypoint runner. """
-    args = parser.parse_args()
+    args = __ROOT_PARSER.parse_args()
 
     with Path(args.config).open() as f:
-        config = yaml.safe_load(f)
+        config_data = yaml.safe_load(f)
+        config = cattrs.structure(config_data, Config)
 
-    # Initialise the portainer connector
     portainer_connector = PortainerHelper(
-        api_key=config["portainer"]["api_key"],
-        portainer_url=config["portainer"]["url"],
+        api_key=config.portainer.api_key,
+        portainer_url=config.portainer.url,
     )
 
-    if args.export:
-        example = portainer_connector.export_config_from_stacks()
-        with Path(args.export).open("w") as f:
-            yaml.dump(example, f, indent=4)
-
-    if args.sync:
-        sync_file = args.sync in ("file", "all")
-        sync_git = args.sync in ("git", "all")
-        portainer_connector.sync_stack_envs(config, sync_file, sync_git)
+    args.entry_func(args, config, portainer_connector)
 
 
 if __name__ == "__main__":
--- homelab_assistant/portainer.py
+++ homelab_assistant/portainer.py
@@ -6,6 +6,8 @@ from typing import Any
 import requests
 from rich.console import Console
 
+from homelab_assistant.config import Config
+
 console = Console()
 
 
@@ -22,28 +24,75 @@ class PortainerHelper:
         self.session = requests.session()
         self.session.headers.update({"X-API-Key": api_key})
 
-    def get_stacks(self) -> list[dict[str, Any]]:
+    def get_stacks(self) -> dict[str, dict[str, Any]]:
         """ Get data on all defined Portainer stacks.
 
         Returns:
-            list[dict[str, Any]]: List of dictionaries containing Portainer stack information.
+            dict[str, dict[str, Any]]: Key-value pairs of stack names to Portainer stack information.
         """
         response = self.session.get(f"{self.portainer_url}/api/stacks")
         response.raise_for_status()
-        return sorted(response.json(), key=lambda stack: stack["Name"])
+        return {stack["Name"]: stack for stack in response.json()}
 
-    def get_stack_compose_file(self, stack_id: int) -> str:
+    def export_config_from_stacks(self) -> dict[str, str]:
+        """ Export a config file with environment information currently present in Portainer's stacks.
+
+        Returns:
+            dict[str, str]: Config file with Portainer's stack environment information.
+        """
+        output = {}
+        for stack in self.get_stacks():
+            if (stack_env := {env["name"]: env["value"].strip('"') for env in stack["Env"]}):
+                output.setdefault(stack["Name"], {"environment": {}})
+                output[stack["Name"]]["environment"] = stack_env
+
+        return {"stacks": output}
+
+    def get_stack_compose_file(self, stack_id: int) -> str | None:
         """ Get the compose file associated with a given stack ID.
 
         Args:
             stack_id (int): Stack ID to get the compose file for.
 
         Returns:
-            str: Compose file data string.
+            str | None: Compose file data string, or None if it did not exist.
         """
-        response = self.session.get(f"{self.portainer_url}/api/stacks/{stack_id}/file")
-        response.raise_for_status()
-        return response.json()["StackFileContent"]
+        try:
+            response = self.session.get(f"{self.portainer_url}/api/stacks/{stack_id}/file")
+            response.raise_for_status()
+            return response.json()["StackFileContent"]
+        except requests.HTTPError:
+            return None
+
+    def get_git_compose_file(self, stack_name: str, config: Config) -> str | None:
+        """ Get the compose file associated a stacks Git config.
+
+        Args:
+            stack_name (str): Name of the stack in config to retrieve compose file for.
+            config (Config): Config object to fet Git config from.
+
+        Returns:
+            str | None: Compose file data string, or None if it did not exist.
+        """
+        stack_git_config = config.stacks.get(stack_name).git
+
+        repository = config.git_default.repository or stack_git_config.repository
+        branch = config.git_default.branch or stack_git_config.branch
+        file_path = stack_git_config.file_path
+
+        if not all((repository, branch, file_path)):
+            return None
+
+        # Create the URL to the raw GitHub compose file
+        url = f"https://raw.githubusercontent.com/{repository.strip('/')}/{branch.strip('/')}/{file_path.strip('.')}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.HTTPError:
+            return None
+
+        return response.text
 
     def get_defined_env_vars(self, compose_file: str) -> list[str]:
         """ Search a compose file for environment variable names which are defined.
@@ -57,91 +106,87 @@ class PortainerHelper:
         return [env_var.strip() for env_var in re.findall(r"\${(.*?)}", compose_file)]
 
     def generate_env_values_from_config(self, required_env_names: list[str],
-                                        config: dict[str, dict[str, str]], stack_name: str) -> dict[str, str]:
+                                        config: Config, stack_name: str) -> dict[str, str]:
         """ Generate environment variable key value pairs defined in config for a given stack.
 
         Args:
             required_env_names (list[str]): Environment variable names required by the compose file.
-            config (dict[str, dict[str, str]]): Config to source common and stack specific \
-                                                environment variable values from.
+            config (Config): Config object to source common and stack specific environment variable values from.
             stack_name (str): Name of the stack to consider.
+
+        Raises:
+            ValueError: No value defined for a given environment variable.
 
         Returns:
             dict[str, str]: Key-value pairs of environment variable names to their values.
         """
         output = {}
         for env in required_env_names:
-            if (value := config["common"].get(env, None)) or (value := config["stacks"][stack_name].get(env, None)):
+            if (
+                (value := config.common_environment.get(env, None)) or
+                (value := config.stacks.get(stack_name).environment.get(env, None))
+            ):
                 # Values are wrapped in double quotes to escape them in portainer properly
                 output[env] = f'"{value}"'
             else:
                 console.print(f"[red]WARNING[/red]: No value defined '{env}' in stack '{stack_name}'")
+                raise ValueError(f"No value defined '{env}' in stack '{stack_name}'")
 
         return output
 
-    def export_config_from_stacks(self) -> dict[str, str]:
-        """ Export a config file with environment information currently present in Portainer's stacks.
-
-        Returns:
-            dict[str, str]: Config file with Portainer's stack environment information.
-        """
-        output = {}
-        for stack in self.get_stacks():
-            if (stack_env := {env["name"]: env["value"].strip('"') for env in stack["Env"]}):
-                output[stack["Name"]] = stack_env
-
-        return output
-
-    def sync_stack_envs(self, config: dict[str, dict[str, str]],
-                        sync_file: bool = False, sync_git: bool = False) -> None:
+    def sync_stacks(self, config: Config) -> None:
         """ Sync Portainer stack environment variable values with values defined in config.
 
+        Raises:
+            NotImplementedError: New deployment feature not implemented.
+            ValueError: Compose file not defined in Git or Portainer.
+
         Args:
-            config (dict[str, dict[str, str]]): Config to source common and stack specific \
-                                                environment variable values from.
-            sync_file (bool, optional): Sync file based stacks. Defaults to False.
-            sync_git (bool, optional): Sync Git based stacks. Defaults to False.
+            config (Config): Config to source common and stack specific environment variable values from.
         """
-        # Exit early if neither sync condition is specified
-        if not sync_file and not sync_git:
-            return
+        portainer_stack_info = self.get_stacks()
 
-        for stack in self.get_stacks():
-            # Extract key information from the given stack data
-            stack_name = stack["Name"]
-            stack_id = stack["Id"]
-            stack_env = {env["name"]: env["value"] for env in stack["Env"]}
-            endpoint_id = stack["EndpointId"]
-            is_git_based = bool(stack["GitConfig"])
+        for stack_name, stack_config in config.stacks.items():
+            # If the stack is not required to be synced, continue
+            if not stack_config.sync:
+                continue
 
-            if stack_name in config["stacks"]:
-                # Skip the stack if the type does not match the sync criteria
-                if (is_git_based and not sync_git) or (not is_git_based and not sync_file):
-                    continue
+            # TODO - Implement deployments without existing stacks
+            if not (portainer_stack := portainer_stack_info.get(stack_name)):
+                raise NotImplementedError("New deployments not currently supported")
 
-                stack_compose = self.get_stack_compose_file(stack_id)
-                required_env_vars = self.get_defined_env_vars(stack_compose)
-                config_env = self.generate_env_values_from_config(required_env_vars, config, stack_name)
+            # Extract stack info from the Portainer response
+            stack_id = portainer_stack["Id"]
+            endpoint_id = portainer_stack["EndpointId"]
 
-                if config_env == stack_env:
-                    console.print(f"[blue]Nothing to do for[/blue] '{stack_name}'")
-                    continue
+            # Fetch both the Git and Portainer compose files if they exist
+            git_compose = self.get_git_compose_file(stack_name, config)
+            portainer_compose = self.get_stack_compose_file(stack_id)
+            if not any((git_compose, portainer_compose)):
+                raise ValueError(f"Compose file not defined in Git or Portainer for stack '{stack_name}'")
 
-                payload = {"env": [
+            # Set the compose file and generate the required environment variables
+            compose = git_compose or portainer_compose
+            required_env_vars = self.get_defined_env_vars(compose)
+            config_env = self.generate_env_values_from_config(required_env_vars, config, stack_name)
+
+            # Check if config or compose file need updating
+            portainer_env = {env["name"]: env["value"] for env in portainer_stack["Env"]}
+            if (config_env == portainer_env) and (compose == portainer_compose):
+                console.print(f"[blue]Nothing to do for[/blue] '{stack_name}'")
+                continue
+
+            # Add required environment variables and compose file to the update payload
+            payload = {
+                "env": [
                     {"name": name, "value": value} for name, value in config_env.items()
-                ]}
+                ],
+                "stackFileContent": compose,
+            }
 
-                # Updates to file based stacks must supply a compose file,
-                # so attach the associated compose file if required
-                if not is_git_based:
-                    payload["stackFileContent"] = stack_compose
-
-                # Change the deploy URL depending on the stack's type
-                deploy_url = (f"{self.portainer_url}/api/stacks/{stack_id}"
-                              f"{'/git/redeploy' if is_git_based else ''}"
-                              f"?endpointId={endpoint_id}")
-
-                console.print(f"Updating {stack_name}... ", end="")
-                response = self.session.put(deploy_url, json=payload)
-                response.raise_for_status()
-                console.print("[green]done[/green]")
+            # Update the stack with the generated payload
+            console.print(f"Updating {stack_name}... ", end="")
+            deploy_url = (f"{self.portainer_url}/api/stacks/{stack_id}?endpointId={endpoint_id}")
+            response = self.session.put(deploy_url, json=payload)
+            response.raise_for_status()
+            console.print("[green]done[/green]")
